6299b8b384d5d3bc83527eb01786716a7fa97d1a
mhm1990 <mayank4185@csu.fullerton.edu>
2016-07-23 14:26:09 -0700

    Trimmed Code

--- FLANN/FLANN.m
+++ FLANN/FLANN.m
@@ -1,64 +1,58 @@
+tic
 clc
 clear all
 close all
 clf('reset')
 clf
 load 'theta.mat'
-
-X = Z(:,1);
-Y = Z(:,2);
-D = THETA;
-D1 = THETA(:,1);
-D2 = THETA(:,2);
-
-
-X_1= X';
-Y_1= Y';
-
+X = Z (:,1); %input data
+Y = Z (:,2); %input data
+D = THETA; %desired output data
+X= X'; %Re-arrange data for training
+Y= Y'; %Re-arrange data for training
 maxepoch = 1000;
-alpha =1;
-MSEmin = 0.010;
-[W,MSE,epoch,PST1,PST2,PS2] = Train_FLANN(X_1,Y_1,D1',D2',alpha,maxepoch,MSEmin);
-%,PSX,PSY
-
+alpha =0.75;
+MSEmin = 0.01;
+[W,MSE,epoch,PST,PS] = Train_FLANN(X,Y,D,alpha,maxepoch,MSEmin);%call to training function
 figure(1)
 plot( epoch,MSE(:,1),'r-');
-semilogy(MSE);
-
 xlabel('No. of epochs');
 ylabel('Mean-Square error for Theta1');
 figure(2)
 plot( epoch,MSE(:,2),'g-');
 xlabel('No. of epochs');
 ylabel('Mean-Square error for Theta2');
-
 %display(W)
-X = Z(350:end,1);
-Y = Z(350:end,2);
-d = D(350:end,:);
-X_3= X';
-Y_3= Y';
-O3 = Run_FLANN(X_3,Y_3,W,PST1,PST2,PS2);%PST1,PST2,PSX,PSY);
-o1 = O3(:,1);
-o2 = O3(:,2);
-sample = 1:size(X_3,2);
-
+X = Z(:,1);
+Y = Z(:,2);
+X= X';
+Y= Y';
+O = Run_FLANN(X,Y,W,PST,PS);
+o1 = O(:,1);
+o2 = O(:,2);
+sample = 1:size(X,2);
+d = D(1:size(X,2),1);
 figure(3)
-plot(sample,O3(:,1),':bs');
+plot(sample,O(:,1),':bs');
 hold on;
-plot(sample,d(:,1),'--mo');
+plot(sample,D(:,1),'--mo');
+xlabel('No of samples');
+ylabel('Theta 1 in radian');
+title('Matching plot for theta1');
 
 figure(4)
-plot(sample,O3(:,2),':bs');
+plot(sample,O(:,2),':bs');
 hold on;
-plot(sample,d(:,2),'--mo');
-X =X_3';
-Y=Y_3';
-fprintf('   X        Y      T1      T2      NNT1     NNT2    ET1     ET2\n');
+plot(sample,D(:,2),'--mo');
+xlabel('No of samples');
+ylabel('Theta 2 in radian');
+title('Matching plot for theta2');
+X =X';
+Y=Y';
+fprintf(' X Y T1 T2 NNT1 NNT2 ET1 ET2\n');
 for i=1:1:size(X,1)
-    fprintf ('%3.4f  %3.4f  %3.4f  %3.4f   %3.4f  %3.4f  %3.4f  %3.4f\n',X(i), Y(i), d(i,:), O3(i,:), d(i,:)-O3(i,:));
+fprintf ('%3.4f %3.4f %3.4f %3.4f %3.4f %3.4f %3.4f %3.4f\n',X(i), Y(i), D(i,:), O(i,:), D(i,:)- O(i,:));
 end
-%P = D-O;
-%save 'test.xls' X Y D O P
-fprintf('\nTraining Set Accuracy: %f\n', mean(double(O3 == d)) * 100);
 
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(O == D)) * 100);
+toc
